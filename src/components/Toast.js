8e9673ed1966cb3b86fdd2eb13000ef40e24d837
Peter <peterycyeh@gmail.com>
2021-11-23 02:20:45 -0800

    Implement a Toast component by using Material UI

---
+++ src/components/Toast.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import styled from '@emotion/styled';
+
+export default function Toast(props) {
+  const { data, open, onClickClose, onClickLike } = props;
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    onClickClose();
+  };
+
+  const renderMessage = (data) => (
+    <MessageWrapper>
+      <span>{data?.firstName} {data?.lastName}</span>
+      <span>{data?.email}</span>
+    </MessageWrapper>
+  );
+
+  const renderAction = (
+    <>
+      <Button color="info" size="small" onClick={onClickLike}>
+        Like
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  );
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      message={renderMessage(data)}
+      action={renderAction}
+    />
+  );
+}
+
+const MessageWrapper = styled.div`
+  > span {
+    display: block;
+    color: silver;
+  }
+`;
